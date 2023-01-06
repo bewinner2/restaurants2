330b5c82fa71d6895e1237374242d22fbdc03b00
bewinner2 <bewinner2@gmail.com>
2023-01-06 12:24:38 +0900

    초기화

--- src/main/java/com/example/restaurant2/wishlist/dto/WishListDto.java
+++ src/main/java/com/example/restaurant2/wishlist/dto/WishListDto.java
@@ -2,17 +2,4 @@ package com.example.restaurant2.wishlist.dto;
 
 public class WishListDto {
 
-
-    //first commit wishListDto
-
-    //second commit wishListDto
-
-    //conflict-test
-    //conflict- 123 첫번째 conflict 시도
-    //conflict- 123 여기는 conflict는 아니고 그냥 추가
-
-
-    private int test;
-    private int test2;
-
 }
